b4711bc333586ef5c610201653623d4274cbbc4d
mgurram <manojkrishna.24@gmail.com>
2016-12-07 22:46:46 -0500

    Initial project structure checked in with 9 test cases

--- tests/com/app/dice/DiceAppTest.java
+++ tests/com/app/dice/DiceAppTest.java
@@ -27,4 +27,68 @@ public class DiceAppTest {
         assertTrue(score == 1200);
     }
 
+    @Test
+    public void diceRollTest3() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{1, 1, 1, 4, 4});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 1080);
+    }
+
+    @Test
+    public void diceRollTest4() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{1, 1, 4, 4, 4});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 600);
+    }
+
+    @Test
+    public void diceRollTest5() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{1, 4, 4, 4, 4});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 540);
+    }
+
+    @Test
+    public void diceRollTest6() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{4, 4, 4, 4, 4});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 480);
+    }
+
+    @Test
+    public void diceRollTest7() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{5, 6, 2, 3, 1});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 100);
+    }
+
+    @Test
+    public void diceRollTest8() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{4, 6, 2, 3, 1});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 140);
+    }
+
+    @Test
+    public void diceRollTest9() throws Exception {
+        List<Integer> diceRoll = Arrays.asList(new Integer[]{1, 1, 1, 4, 1});
+        DiceApp diceApp = new DiceApp();
+        int score = diceApp.diceScore(diceRoll);
+        System.out.println("Score : " + score);
+        assertTrue(score == 1140);
+    }
+
+
 }
